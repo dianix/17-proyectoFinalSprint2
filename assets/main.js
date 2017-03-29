e16cefc6f8ceca0b04a1da658904de181b687caf
Diana <dianavazquez.dcg@gmail.com>
2017-03-29 13:58:55 -0600

    intento de boton borrar fallido

--- assets/main.js
+++ assets/main.js
@@ -1,6 +1,15 @@
 //alert("Help! I need somebody... Help!");
 
 // ESTOY PERDIDA! va de nuez
+
+// clase constructora Lista (aun no se como usarla)
+/*function Lista(){
+    this.nombre = nombre;
+    this.listaVacia = lista;
+    this.pendiente = pendiente;
+    this.borrarLista = 
+}*/
+
 // variable global para guardar listas ¿?
 var listas = [];
 
@@ -9,9 +18,10 @@ var listas = [];
 function crearLista(){
     //creando contenedor para elementos de la lista
     var elementoLista = document.createElement("div");
-    var idElementoLista = document.createAttribute("id");
-    idElementoLista.value = Date.now();
-    elementoLista.setAttributeNode(idElementoLista);
+    elementoLista.id = Date.now();
+    var idElementoLista = elementoLista.id;
+    console.log(idElementoLista);
+    
     
     //para darle nombre a lista
     var nombreElementoLista = document.createElement("h3");
@@ -26,21 +36,32 @@ function crearLista(){
     lista.setAttributeNode(idLista);
     elementoLista.appendChild(lista);
     
+    // input para pendientes
+    var input = document.createElement("input");
+    elementoLista.appendChild(input);
+    
     // crear boton agregar pendiente
     var botonAgregar = document.createElement("button");
     var idBoton = document.createAttribute("id");
     idBoton.value = Date.now();
     botonAgregar.setAttributeNode(idBoton);
-    botonAgregar.addEventListener("click",agregarPendiente());
+    botonAgregar.onclick = agregarPendiente();
     botonAgregar.innerText = "Agregar pendiente";
     elementoLista.appendChild(botonAgregar);
     // boton borrar lista
     var botonBorrarL = document.createElement("button");
-    var idBotonbotonBorrarL = document.createAttribute("id");
-    idBotonbotonBorrarL.value = Date.now();
-    botonBorrarL.setAttributeNode(idBotonbotonBorrarL);
-    botonBorrarL.addEventListener("click",borrarLista());
+    botonBorrarL.id = Date.now();
+    botonBorrarL.value = idElementoLista; 
+    console.log(botonBorrarL.value);
     botonBorrarL.innerText = "Borrar lista";
+    botonBorrarL.onclick = function(){
+        var elemento = document.getElementById(idElementoLista);
+        elemento.removeChild(this);
+        elemento.removeChild(document.getElementById(this.value));
+    };
+    
+    // 1Ñ30 mi cerebro está fundidooooooo!!! T__T no carburo mas... pero se ve chido 
+    
     elementoLista.appendChild(botonBorrarL);
     
     //llamando a contenedor de listas
@@ -49,9 +70,8 @@ function crearLista(){
 }
 
 function agregarPendiente(){
-    
-}
-
-function borrarLista(){
-    
+    // 
+    var contenido = document.getElementById
+    var pendienteNuevo = document.createElement("li");
+     
 }
