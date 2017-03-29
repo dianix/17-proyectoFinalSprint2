d01e0350641555a96f46045248a34548ac67a0c8
Diana <dianavazquez.dcg@gmail.com>
2017-03-29 12:26:07 -0600

    Se crean listas con botones que aun no sirven

--- assets/main.js
+++ assets/main.js
@@ -1,23 +1,57 @@
 //alert("Help! I need somebody... Help!");
 
+// ESTOY PERDIDA! va de nuez
+// variable global para guardar listas ¿?
+var listas = [];
+
+
+
 function crearLista(){
     //creando contenedor para elementos de la lista
-    var listaContenida = document.createElement("div");
-    var idLista = document.createAttribute("id");
-    idLista.value = Date.now();
-    listaContenida.setAttributeNode(idLista);
+    var elementoLista = document.createElement("div");
+    var idElementoLista = document.createAttribute("id");
+    idElementoLista.value = Date.now();
+    elementoLista.setAttributeNode(idElementoLista);
     
     //para darle nombre a lista
-    var nombreLista = document.createElement("h3");
+    var nombreElementoLista = document.createElement("h3");
     var nombreDado = document.createTextNode(prompt("Nombre de lista"));
-    nombreLista.appendChild(nombreDado);
-    listaContenida.appendChild(nombreLista);
-   
+    nombreElementoLista.appendChild(nombreDado);
+    elementoLista.appendChild(nombreElementoLista);
     
+    // para crear la lista en si
+    var lista = document.createElement("ul");
+    var idLista = document.createAttribute("id");
+    idLista.value = Date.now();
+    lista.setAttributeNode(idLista);
+    elementoLista.appendChild(lista);
+    
+    // crear boton agregar pendiente
+    var botonAgregar = document.createElement("button");
+    var idBoton = document.createAttribute("id");
+    idBoton.value = Date.now();
+    botonAgregar.setAttributeNode(idBoton);
+    botonAgregar.addEventListener("click",agregarPendiente());
+    botonAgregar.innerText = "Agregar pendiente";
+    elementoLista.appendChild(botonAgregar);
+    // boton borrar lista
+    var botonBorrarL = document.createElement("button");
+    var idBotonbotonBorrarL = document.createAttribute("id");
+    idBotonbotonBorrarL.value = Date.now();
+    botonBorrarL.setAttributeNode(idBotonbotonBorrarL);
+    botonBorrarL.addEventListener("click",borrarLista());
+    botonBorrarL.innerText = "Borrar lista";
+    elementoLista.appendChild(botonBorrarL);
     
     //llamando a contenedor de listas
     var contenedorListas = document.getElementById("contenedorListas");
-    contenedorListas.appendChild(listaContenida);
+    contenedorListas.appendChild(elementoLista);
+}
+
+function agregarPendiente(){
     
+}
+
+function borrarLista(){
     
 }
