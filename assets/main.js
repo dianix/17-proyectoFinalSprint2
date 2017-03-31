ff1ecc03f0c8730569bf994beb08422ae5751c83
Diana Vázquez <dianavazquez.dcg@gmail.com>
2017-03-31 00:33:51 -0600

    Segunda oportunidad para el ejercicio - con más éxito

--- assets/main.js
+++ assets/main.js
@@ -1,77 +1,130 @@
 //alert("Help! I need somebody... Help!");
 
-// ESTOY PERDIDA! va de nuez
 
-// clase constructora Lista (aun no se como usarla)
-/*function Lista(){
-    this.nombre = nombre;
-    this.listaVacia = lista;
-    this.pendiente = pendiente;
-    this.borrarLista = 
-}*/
+// *** INTENTO 2.0 *** ---> Ajusté mi código inicial para aplicar el POO, creo que obtuve un buen resultado
 
 // variable global para guardar listas ¿?
 var listas = [];
 
+// función constructora Lista
+function Lista(id,nombre){
+    this.id = id;
+    this.nombre = nombre;
+    this.pendientes = [];
+}
 
+// función constructora pendiente
+function Pendiente(id,contenido){
+    this.id = id;
+    this.contenido = contenido;
+}
+//-----------------------------------------------------------------------------
 
+// para crear nuevo objeto lista    
 function crearLista(){
+    var nombreLista = document.getElementById("nombreLista").value;
+    //console.log(nombreLista);
+    var idLista = Date.now();
+    var nuevaLista = new Lista(idLista,nombreLista);
+    
+    // guardo nueva lista en arreglo global
+    listas.push(nuevaLista);
+    //console.log(listas);
+    
+    // llamo funciones para vaciar input y mostrar la lista creada
+    borrarInput(nombreLista);
+    mostrarNuevaLista(nuevaLista);
+}
+//----------------------------------------------------------------------------
+
+// para borrar inputs de texto
+function borrarInput(elemento){
+    var inputABorrar = elemento;
+    nuevoValor = "";
+    inputABorrar.value = nuevoValor;
+}
+//-----------------------------------------------------------------------------
+
+// para mostrar listas en html
+function mostrarNuevaLista(nuevaLista){
     //creando contenedor para elementos de la lista
     var elementoLista = document.createElement("div");
-    elementoLista.id = Date.now();
-    var idElementoLista = elementoLista.id;
-    console.log(idElementoLista);
-    
-    
+        
     //para darle nombre a lista
     var nombreElementoLista = document.createElement("h3");
-    var nombreDado = document.createTextNode(prompt("Nombre de lista"));
-    nombreElementoLista.appendChild(nombreDado);
+    nombreElementoLista.innerText = nuevaLista.nombre;
     elementoLista.appendChild(nombreElementoLista);
     
-    // para crear la lista en si
+    
+    // boton borrar lista
+    var botonBorrarLista = document.createElement("button");
+    botonBorrarLista.innerText = "Borrar lista";
+    botonBorrarLista.onclick = function(){
+        this.parentNode.parentNode.removeChild(this.parentNode);
+    };
+    elementoLista.appendChild(botonBorrarLista);
+    
+    // para crear la lista en sí
     var lista = document.createElement("ul");
     var idLista = document.createAttribute("id");
     idLista.value = Date.now();
     lista.setAttributeNode(idLista);
     elementoLista.appendChild(lista);
     
-    // input para pendientes
+    // input para pendientes a agregar
     var input = document.createElement("input");
+    input.placeholder = "Nuevo pendiente";
     elementoLista.appendChild(input);
     
     // crear boton agregar pendiente
     var botonAgregar = document.createElement("button");
-    var idBoton = document.createAttribute("id");
-    idBoton.value = Date.now();
-    botonAgregar.setAttributeNode(idBoton);
-    botonAgregar.onclick = agregarPendiente();
+    botonAgregar.onclick = crearPendiente;
     botonAgregar.innerText = "Agregar pendiente";
     elementoLista.appendChild(botonAgregar);
-    // boton borrar lista
-    var botonBorrarL = document.createElement("button");
-    botonBorrarL.id = Date.now();
-    botonBorrarL.value = idElementoLista; 
-    console.log(botonBorrarL.value);
-    botonBorrarL.innerText = "Borrar lista";
-    botonBorrarL.onclick = function(){
-        var elemento = document.getElementById(idElementoLista);
-        elemento.removeChild(this);
-        elemento.removeChild(document.getElementById(this.value));
-    };
-    
-    // 1Ñ30 mi cerebro está fundidooooooo!!! T__T no carburo mas... pero se ve chido 
-    
-    elementoLista.appendChild(botonBorrarL);
     
     //llamando a contenedor de listas
     var contenedorListas = document.getElementById("contenedorListas");
     contenedorListas.appendChild(elementoLista);
 }
+//-----------------------------------------------------------------------------
 
-function agregarPendiente(){
-    // 
-    var contenido = document.getElementById
+// para crear nuevo pendiente
+function crearPendiente(){
+    var id = Date.now();
+    var cajaTexto = this.previousSibling;
+    var contenidoPendiente = cajaTexto.value;
+    //console.log(contenidoPendiente);
+    var nuevoPendiente = new Pendiente(id,contenidoPendiente);
+    //console.log(cajaTexto,nuevoPendiente);
+    
+    borrarInput(cajaTexto); // servía esta función y ya no ¿?
+    mostrarPendiente(cajaTexto,nuevoPendiente);
+};
+
+// para mandar pendiente a lista de html
+function mostrarPendiente(cajaTexto,nuevoPendiente) {
+    //lista a la que voy a agregar el pendiente
+    var lista = cajaTexto.previousSibling;
     var pendienteNuevo = document.createElement("li");
-     
+    pendienteNuevo.innerText = nuevoPendiente.contenido;
+    
+    var botonBorrarPendiente = document.createElement("button");
+    botonBorrarPendiente.innerText = "x";
+    botonBorrarPendiente.onclick = function(){
+        var lista = this.parentNode;
+        lista.removeChild(this.previousSibling); 
+        lista.removeChild(this);
+    };
+    
+    lista.appendChild(pendienteNuevo);
+    lista.appendChild(botonBorrarPendiente);
+    
 }
+
+
+
+
+
+
+
+
